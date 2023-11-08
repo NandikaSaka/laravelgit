a2b7cab75abd3c046e986f08d429277509f14b6e
Made Nandika Saka Putra <115137019+NandikaSaka@users.noreply.github.com>
2023-11-08 16:09:37 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::resource('mahasiswaData', MahasiswaDataController::class);
+
+Route::get('/', function (){
+    return redirect()->route('mahasiswaData');
+});
