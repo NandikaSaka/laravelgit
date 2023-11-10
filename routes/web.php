bbcddd760aab3a01fd4778603bc49d940994074b
NandikaSaka <putra.2205551006@student.unud.ac.id>
2023-11-10 22:31:11 +0800

    update 10/11_3

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('mahasiswaData', MahasiswaDataController::class);
 
-Route::get('mahasiswaData', 'MahasiswaDataController@index');
-
 Route::get('/', [MahasiswaDataController::class, 'index']);
