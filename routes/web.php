adde36628d0f38cde6f85a5cedf6eced58762a86
NandikaSaka <putra.2205551006@student.unud.ac.id>
2023-12-03 16:20:28 +0800

    fix route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('mahasiswaData', MahasiswaDataController::class);
+Route::resource('mahasiswaData', MahasiswaDataController::class)->names('mahasiswaData');
 
 Route::get('/', [MahasiswaDataController::class, 'index']);
